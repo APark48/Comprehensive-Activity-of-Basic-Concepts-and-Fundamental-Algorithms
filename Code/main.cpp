17582dfc0b8028c1692dc472f17be23da33fae77
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-09-27 17:43:25 -0500

    Updated Reader.h

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,10 +1,17 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Developed by Valter Alejandro Kuhne Hernández (A01379392)
 //Copyright © 2020. All rights reserved.
-#include "Reader.h"
+//#include "Reader.h"
+#include "Sorter.h"
 #include <iostream>
 
 int main(){
-    
+    std::vector <int> v = {5,4,6,78,92,1,40};
+    Quicksort <int> q;
+    q.quicksort(v, 1, 8);
+    for (auto i:v){
+        std::cout << i << " ";
+    }
+
     return 0;
 }
