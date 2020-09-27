1fb7d25f112d3daec5ebf91a618015d8801b9314
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-09-27 17:07:13 -0500

    Updated Reader.h

--- Code/Reader.h
+++ Code/Reader.h
@@ -1,16 +1,24 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Developed by Valter Alejandro Kuhne Hernández (A01379392)
 //Copyright © 2020. All rights reserved.
-#pragma once
+#pragma  once
 #include "Data.h"
 #include <fstream>
 #include <vector>
 
+std::string date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname;
 std::vector <class Data> read(){
-    std::string date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname;
     std::ifstream data("equipo11.csv");
     while(data.peek()!=EOF){
         std::getline(data, date, ',');
         std::getline(data, time, ',');
+        std::getline(data, sourceIp, ',');
+        std::getline(data, sourcePort, ',');
+        std::getline(data, sourceHostname, ',');
+        std::getline(data, destinationIp, ',');
+        std::getline(data, destinationPort, ',');
+        std::getline(data, destinationHostname, '\n');
+        Data r(date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname);
+        read.push_back(r);
     }
 }
