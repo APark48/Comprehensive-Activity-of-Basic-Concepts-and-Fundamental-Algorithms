fc5f52ed46f786c9ff03e8f7f0fa9b610376d931
Asdertuy <57368258+Asdertuy@users.noreply.github.com>
2020-09-28 11:05:25 -0500

    updated Reader.h and Sorter.h

--- Code/Searcher.h
+++ Code/Searcher.h
@@ -2,21 +2,19 @@
 //Developed by Valter Alejandro Kuhne Hernández (A01379392)
 //Copyright © 2020. All rights reserved.
 #pragma once
-#include "Data.h"
-#include <iostream>
 #include <vector>
-
+#include "Data.h"
 template <typename T>
-int binarySearch(int start, int end, T value, std::vector<class Data<T>> &elements){
-    if(end>=start){
-        int mid=start + (start-end)/2;
-        if(elements.at(mid).getSourceHostname()==value){
-            return mid;
+int binarySearch(int inicio, int final, T valor, std::vector<T> &valores){
+    if(final>=inicio){
+        int i=inicio + (final-inicio)/2;
+        if(valores.at(i)==valor){
+            return i;
         } 
-        if (elements.at(mid).getSourceHostname()>value){
-            return binarySearch(start, mid-1, value, elements);
+        if (valores.at(i)>valor){
+            return binarySearch(inicio, i-1, valor, valores);
         }
-        return binarySearch(mid+1, end, value, elements);
+        return binarySearch(i+1, final, valor, valores);
     }
     return -1;
 }
