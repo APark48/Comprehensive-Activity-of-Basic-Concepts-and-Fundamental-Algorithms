f7433c34e48c8b392ffa189db494b14b277fcc01
Asdertuy <57368258+Asdertuy@users.noreply.github.com>
2020-09-27 21:59:05 -0500

    updated Searcher.h

---
+++ Code/Searcher.h
@@ -0,0 +1,21 @@
+//Developed by Alfredo Jeong Hyun Park (A01658259)
+//Developed by Valter Alejandro Kuhne Hernández (A01379392)
+//Copyright © 2020. All rights reserved.
+#include "Data.h"
+#include <iostream>
+#include <vector>
+
+template <typename T>
+int binarySearch(int inicio, int final, T valor, std::vector<class Data<T>> &valores){
+    if(final>=inicio){
+        int i=inicio + (final-inicio)/2;
+        if(valores.at(i).getSourceHostname()==valor){
+            return i;
+        } 
+        if (valores.at(i).getSourceHostname()>valor){
+            return binarySearch(inicio, i-1, valor, valores);
+        }
+        return binarySearch(i+1, final, valor, valores);
+    }
+    return -1;
+}
--- Code/main.cpp
+++ Code/main.cpp
@@ -1,17 +1,33 @@
 //Developed by Alfredo Jeong Hyun Park (A01658259)
 //Developed by Valter Alejandro Kuhne Hernández (A01379392)
 //Copyright © 2020. All rights reserved.
-//#include "Reader.h"
+#include "Reader.h"
 #include "Sorter.h"
+#include "Searcher.h"
+#include "Data.h"
 #include <iostream>
 
+bool compareHostnameF (Data<std::string> &a, Data<std::string> &b){
+     return 0;
+}
+
+bool compareIPFuente(Data<std::string> &a, Data<std::string> &b){
+    return 0;
+};
+
+
 int main(){
-    std::vector <int> v = {5,4,6,78,92,1,40};
-    Quicksort <int> q;
-    q.quicksort(v, 1, 8);
-    for (auto i:v){
-        std::cout << i << " ";
-    }
+    std::vector<class Data<std::string>> datos;
+    datos=readRecords();
+    int dia2;
+    dia2=segundoDia(datos);
+    std::cout << dia2 << std::endl;
+    Sorter<Data<std::string>> organizar;
+    organizar.ordenaQuick(datos, &compareHostNameFuente);
+    countNames(datos);
+    organizar.ordenaQuick(datos, &compareIPFuente);
+    direccionIP(datos);
+    
 
     return 0;
 }
