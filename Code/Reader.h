64b0f555c756e22b3ada09c4dba4c3bb18bb363a
Alfredo Jeong Hyun Park <alfredpark887@hotmail.com>
2020-09-27 19:54:44 -0500

    Updated Reader.h

--- Code/Reader.h
+++ Code/Reader.h
@@ -3,14 +3,15 @@
 //Copyright © 2020. All rights reserved.
 #pragma once
 #include "Data.h"
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 
 using std::getline;
 
-std::string date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname;
 std::vector <class Data<std::string>> reader(){
+    std::string date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname;
     std::vector <class Data<std::string>> read;
     std::ifstream data("equipo11.csv");
     while(data.peek()!=EOF){
@@ -25,5 +26,13 @@ std::vector <class Data<std::string>> reader(){
         Data r(date, time, sourceIp, sourcePort, sourceHostname, destinationIp, destinationPort, destinationHostname);
         read.push_back(r);
     }
+    int count=0;
+    for(int i=0; i<read.size(); i++){
+        count+=1;
+    }
+
+    std::cout << "El archivo tiene " << count << " registros." << std::endl;
+    data.close();
+    return read;
 }
 
