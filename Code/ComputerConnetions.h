34419efe6c55210348047140f00d905cc2b2fb56
Asdertuy <57368258+Asdertuy@users.noreply.github.com>
2020-10-13 16:22:37 -0500

    updated ComputerConnections.h

--- Code/ComputerConnetions.h
+++ Code/ComputerConnetions.h
@@ -1,4 +1,36 @@
 #pragma once
 #include <stdlib.h>
 #include <iostream>
+#include "Data.h"
 
+template <typename T>
+class ComputerConnections {
+    private:
+    T IP;
+    T name;
+    T sourceConnections;
+    T destinationConnections;
+    
+    public:
+    ComputerConnections(std::string sourceHostname){
+
+    }
+    ~ComputerConnections(){}
+    
+    T sourceConnections(int destinationIP){
+        
+    }
+
+    T destinationConnections(int sourceIP){
+
+    }
+
+
+    T createNewSourceIP(int n){
+
+    }
+
+    
+
+
+};
